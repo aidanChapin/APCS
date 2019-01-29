0b38e01ba8d0e842a193943d602d1f770c89e30f
Aidan Chapin <32720594+aidanChapin@users.noreply.github.com>
2019-01-28 22:05:20 -0500

    Finished personality test but actually

--- src/Projects/PersonalityTest.java
+++ src/Projects/PersonalityTest.java
@@ -6,9 +6,25 @@ import java.io.PrintStream;
 import java.util.Scanner;
 public class PersonalityTest {
     public static void main(String[] args) throws FileNotFoundException {
+       startUp();
+    }
+    public static void startUp()throws FileNotFoundException{
         Scanner scan = new Scanner(System.in);
         System.out.print("Input file name: ");
         File input = new File(scan.next());
+        boolean fileFound = false;
+        if(input.exists()){
+
+        }
+        else {
+            while (fileFound == false) {
+                System.out.print("File not found. Try again: ");
+                input = new File(scan.next());
+                if (input.exists()) {
+                    fileFound = true;
+                }
+            }
+        }
         System.out.print("Output file name: ");
         File output = new File(scan.next());
         readFile(input, output);
@@ -72,7 +88,6 @@ public class PersonalityTest {
             printStream.println();
         }
     }
-
     public static void calculateType(int[] answers, PrintStream printStream)throws FileNotFoundException{
         String[] type = new String[4];
         int[] percents = new int[4];
@@ -80,8 +95,12 @@ public class PersonalityTest {
             type[0] = "E";
             percents[0] = answers[1]*100/(answers[0]+answers[1]);
         }
-        else{
+        else if (answers[1] > answers[0]){
             type[0] = "I";
+            percents[0] = answers[1]*100/(answers[0]+answers[1]);
+        }
+        else{
+            type[0] = "X";
             percents[0] = answers[1]*100/(answers[0]+answers[1]);
         }
 
@@ -89,8 +108,12 @@ public class PersonalityTest {
             type[1] = "S";
             percents[1] = answers[3] * 100 / (answers[2] + answers[3]);
         }
-        else{
+        else if (answers[3] > answers[2]){
             type[1] = "N";
+            percents[1] = answers[3]*100/(answers[2]+answers[3]);
+        }
+        else{
+            type[1] = "X";
             percents[1] = answers[3]*100/(answers[2]+answers[3]);
         }
 
@@ -98,8 +121,12 @@ public class PersonalityTest {
             type[2] = "T";
             percents[2] = answers[5]*100/(answers[4]+answers[5]);
         }
-        else{
+        else if (answers[5] > answers[4]){
             type[2] = "F";
+            percents[2] = answers[5]*100/(answers[4]+answers[5]);
+        }
+        else{
+            type[2] = "X";
             percents[2] = answers[5]*100/(answers[4]+answers[5]);
         }
 
@@ -107,8 +134,12 @@ public class PersonalityTest {
             type[3] = "J";
             percents[3] = answers[7]*100/(answers[6]+answers[7]);
         }
-        else{
+        else if (answers[7] > answers[6]){
             type[3] = "P";
+            percents[3] = answers[7]*100/(answers[6]+answers[7]);
+        }
+        else{
+            type[3] = "X";
             percents[3] = answers[7]*100/(answers[6]+answers[7]);
         }
         printStream.println("[" + percents[0]+"%, " + percents[1] + "%, " + percents[2]+"%, " + percents[3] + "%] = " + type[0]+type[1]+type[2]+type[3]);
